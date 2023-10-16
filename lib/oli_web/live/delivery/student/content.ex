5bb65b7993fc4af368a4cc1bcb4021a486d22773
Eli Knebel <eliknebel@cmu.edu>
2023-10-16 16:45:20 -0400

    migrate remaining content tab

--- lib/oli_web/live/delivery/student/content.ex
+++ lib/oli_web/live/delivery/student/content.ex
@@ -3,8 +3,36 @@ defmodule OliWeb.Delivery.Student.Content do
 
   import OliWeb.Components.Delivery.Layouts
 
+  alias Oli.Delivery.Sections
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    section =
+      socket.assigns.section
+      |> Oli.Repo.preload([:base_project, :root_section_resource])
+
+    hierarchy = %{"children" => Sections.build_hierarchy(section).children}
+    current_position = 0
+    current_level = 0
+
+    # Adjust the hierarchy in the case that there are only pages in the course, no containers
+    hierarchy =
+      OliWeb.Components.Delivery.CourseContent.adjust_hierarchy_for_only_pages(hierarchy)
+
+    scheduled_dates =
+      Sections.get_resources_scheduled_dates_for_student(
+        section.slug,
+        socket.assigns.ctx.user.id
+      )
+
+    {:ok,
+     assign(socket,
+       hierarchy: hierarchy,
+       current_level_nodes: hierarchy["children"],
+       current_position: current_position,
+       current_level: current_level,
+       scheduled_dates: scheduled_dates,
+       breadcrumbs_tree: [{current_level, current_position, "Curriculum"}]
+     )}
   end
 
   def render(assigns) do
@@ -16,8 +44,21 @@ defmodule OliWeb.Delivery.Student.Content do
       preview_mode={@preview_mode}
       active_tab={:content}
     >
-      <div class="container mx-auto">
-        Content
+      <div class="container mx-auto px-10 py-8">
+        <.live_component
+          module={OliWeb.Components.Delivery.CourseContent}
+          id="course_content_tab"
+          ctx={@ctx}
+          hierarchy={@hierarchy}
+          current_position={@current_position}
+          current_level={@current_level}
+          current_level_nodes={@current_level_nodes}
+          breadcrumbs_tree={@breadcrumbs_tree}
+          section={@section}
+          scheduled_dates={@scheduled_dates}
+          current_user_id={@ctx.user.id}
+          preview_mode={@preview_mode}
+        />
       </div>
     </.header_with_sidebar_nav>
     """
