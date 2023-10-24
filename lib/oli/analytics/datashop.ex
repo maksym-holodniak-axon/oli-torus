030344e5982efe1de7365891220e8a560b3710bf
Eli Knebel <eliknebel@cmu.edu>
2023-10-24 09:50:55 -0400

    do not timeout on datashop query

--- lib/oli/analytics/datashop.ex
+++ lib/oli/analytics/datashop.ex
@@ -52,85 +52,87 @@ defmodule Oli.Analytics.Datashop do
   end
 
   defp create_messages(project_id) do
-    project = Course.get_project!(project_id)
-    publication = Publishing.get_latest_published_publication_by_slug(project.slug)
-    dataset_name = Utils.make_dataset_name(project.slug)
+    Repo.transaction(fn ->
+      project = Course.get_project!(project_id)
+      publication = Publishing.get_latest_published_publication_by_slug(project.slug)
+      dataset_name = Utils.make_dataset_name(project.slug)
 
-    # Fetch context information such as hierarchy revisions, skill titles and
-    # registered activity slugs ahead of time and place them into the context
+      # Fetch context information such as hierarchy revisions, skill titles and
+      # registered activity slugs ahead of time and place them into the context
 
-    # a map of resource ids to published revision
-    hierarchy_map =
-      Publishing.get_published_resources_by_publication(publication.id)
-      |> Enum.reduce(%{}, fn pr, m -> Map.put(m, pr.resource_id, pr.revision) end)
-      |> build_hierarchy_map(publication.root_resource_id)
+      # a map of resource ids to published revision
+      hierarchy_map =
+        Publishing.get_published_resources_by_publication(publication.id)
+        |> Enum.reduce(%{}, fn pr, m -> Map.put(m, pr.resource_id, pr.revision) end)
+        |> build_hierarchy_map(publication.root_resource_id)
 
-    activity_types =
-      Oli.Activities.list_activity_registrations()
-      |> Enum.reduce(%{}, fn a, m -> Map.put(m, a.id, a) end)
+      activity_types =
+        Oli.Activities.list_activity_registrations()
+        |> Enum.reduce(%{}, fn a, m -> Map.put(m, a.id, a) end)
 
-    skill_titles =
-      Oli.Publishing.get_published_objective_details(publication.id)
-      |> Enum.reduce(%{}, fn a, m -> Map.put(m, a.resource_id, a.title) end)
+      skill_titles =
+        Oli.Publishing.get_published_objective_details(publication.id)
+        |> Enum.reduce(%{}, fn a, m -> Map.put(m, a.resource_id, a.title) end)
 
-    Attempts.get_part_attempts_and_users(project.id)
-    |> group_part_attempts_by_user_and_part
-    |> Enum.map(fn {{email, sub, activity_slug, part_id}, part_attempts} ->
-      context = %{
-        date: hd(part_attempts).activity_attempt.resource_attempt.inserted_at,
-        email: email,
-        sub: sub,
-        # datashop_session_id will be set to the latest part attempt datashop_session_id
-        # unless it is nil, then it will be set to a generated UUID. This will handle any
-        # part attempts that existed before this value was tracked in the part attempt record
-        datashop_session_id: value_or(hd(part_attempts).datashop_session_id, UUID.uuid4()),
-        context_message_id: Utils.make_unique_id(activity_slug, part_id),
-        problem_name: Utils.make_problem_name(activity_slug, part_id),
-        dataset_name: dataset_name,
-        part_attempt: hd(part_attempts),
-        records: part_attempts,
-        publication: publication,
-        project: project,
-        hierarchy_map: hierarchy_map,
-        activity_types: activity_types,
-        skill_titles: skill_titles
-      }
+      Attempts.get_part_attempts_and_users(project.id)
+      |> group_part_attempts_by_user_and_part
+      |> Enum.map(fn {{email, sub, activity_slug, part_id}, part_attempts} ->
+        context = %{
+          date: hd(part_attempts).activity_attempt.resource_attempt.inserted_at,
+          email: email,
+          sub: sub,
+          # datashop_session_id will be set to the latest part attempt datashop_session_id
+          # unless it is nil, then it will be set to a generated UUID. This will handle any
+          # part attempts that existed before this value was tracked in the part attempt record
+          datashop_session_id: value_or(hd(part_attempts).datashop_session_id, UUID.uuid4()),
+          context_message_id: Utils.make_unique_id(activity_slug, part_id),
+          problem_name: Utils.make_problem_name(activity_slug, part_id),
+          dataset_name: dataset_name,
+          part_attempt: hd(part_attempts),
+          records: part_attempts,
+          publication: publication,
+          project: project,
+          hierarchy_map: hierarchy_map,
+          activity_types: activity_types,
+          skill_titles: skill_titles
+        }
 
-      [
-        Context.setup("START_PROBLEM", context)
-        | part_attempts
-          |> Enum.flat_map(fn part_attempt ->
-            context =
-              Map.merge(
-                context,
-                %{
-                  date: part_attempt.date_submitted,
-                  transaction_id: Utils.make_unique_id(activity_slug, part_id),
-                  part_attempt: part_attempt,
-                  skill_ids:
-                    part_attempt.activity_attempt.revision.objectives[part_attempt.part_id] || [],
-                  total_hints_available: get_hints_for_part(part_attempt) |> length
-                }
-              )
+        [
+          Context.setup("START_PROBLEM", context)
+          | part_attempts
+            |> Enum.flat_map(fn part_attempt ->
+              context =
+                Map.merge(
+                  context,
+                  %{
+                    date: part_attempt.date_submitted,
+                    transaction_id: Utils.make_unique_id(activity_slug, part_id),
+                    part_attempt: part_attempt,
+                    skill_ids:
+                      part_attempt.activity_attempt.revision.objectives[part_attempt.part_id] || [],
+                    total_hints_available: get_hints_for_part(part_attempt) |> length
+                  }
+                )
 
-            hint_message_pairs =
-              create_hint_message_pairs(
-                part_attempt,
-                context
-              )
+              hint_message_pairs =
+                create_hint_message_pairs(
+                  part_attempt,
+                  context
+                )
 
-            # Attempt / Result pairs must have a different transaction ID from the hint message pairs
-            context =
-              Map.put(context, :transaction_id, Utils.make_unique_id(activity_slug, part_id))
+              # Attempt / Result pairs must have a different transaction ID from the hint message pairs
+              context =
+                Map.put(context, :transaction_id, Utils.make_unique_id(activity_slug, part_id))
 
-            hint_message_pairs ++
-              [
-                Tool.setup("ATTEMPT", "ATTEMPT", context),
-                Tutor.setup("RESULT", context)
-              ]
-          end)
-      ]
-    end)
+              hint_message_pairs ++
+                [
+                  Tool.setup("ATTEMPT", "ATTEMPT", context),
+                  Tutor.setup("RESULT", context)
+                ]
+            end)
+        ]
+      end)
+    end, timeout: :infinity)
   end
 
   # safely get the hints list from a part, returning an empty list if
